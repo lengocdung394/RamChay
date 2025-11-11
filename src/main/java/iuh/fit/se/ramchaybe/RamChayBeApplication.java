b5c081bf881260d4461624ddb35d8527e732469d
NgocDung (Co Phuong Hang) <Dungheongunhubo>
2025-11-11 08:54:19 +0700

    Initial commit for create project

---
+++ src/main/java/iuh/fit/se/ramchaybe/RamChayBeApplication.java
@@ -0,0 +1,13 @@
+package iuh.fit.se.ramchaybe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RamChayBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RamChayBeApplication.class, args);
+    }
+
+}
